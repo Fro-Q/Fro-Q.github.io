57572b9cc320b6e32384e7a2134267f4dfa2776e
Fro-Q <froqqq@outlook.com>
2025-04-15 01:27:40 +0800

    refactor: config ESLint for supporing UnoCSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    unocss: true,
     rules: {
       'vue/max-attributes-per-line': ['error', {
         singleline: { max: 1 },
