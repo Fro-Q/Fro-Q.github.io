dada3ad054dded0285bd1d681bcb6379efc68c7d
Fro-Q <froqqq@outlook.com>
2025-09-10 09:23:29 +0800

    fix: fix hydration mismatch in tag page

--- docs/.vitepress/theme/components/PageContentTag.vue
+++ docs/.vitepress/theme/components/PageContentTag.vue
@@ -38,49 +38,51 @@ const metaStrings = computed(() => [
     <PostMetaInfo :meta-strings="metaStrings" />
   </un-page-content>
 
-  <PostListSection
-    category="在此"
-    :posts="postsToShow['在此']"
-    :excerpt-visible="excerptVisible['在此']"
-    :get-all-years="getAllYears"
-    title="在此"
-    :show-excerpt-toggle="true"
-    @update:excerpt-visible="value => excerptVisible['在此'] = value"
-  >
-    <template #empty-message-addons>
-      <div
-        un-text="2xl neutral-800 dark:neutral-200"
-        un-my-10
-        un-flex="~ row"
-        un-justify-center
-      >
-        此处没有任何文章
-      </div>
-      <div
-        un-text="2xl neutral-700 dark:neutral-300"
-        un-my-10
-        un-flex="~ row"
-        un-justify-center
-      >
-        很可能是由于「{{ params?.tag }}」是一个泛类
-      </div>
-      <div
-        un-text="2xl neutral-600 dark:neutral-400"
-        un-my-10
-        un-flex="~ row"
-        un-justify-center
-      >
-        您需要看看
-        <LinkUnderline
-          un-align-base
-          href="#更深处"
-          text="更深处"
-          un-text="cyan-500"
-          un-before="bg-cyan-600 dark:bg-cyan-600/80"
-        />
-      </div>
-    </template>
-  </PostListSection>
+  <ClientOnly>
+    <PostListSection
+      category="在此"
+      :posts="postsToShow['在此']"
+      :excerpt-visible="excerptVisible['在此']"
+      :get-all-years="getAllYears"
+      title="在此"
+      :show-excerpt-toggle="true"
+      @update:excerpt-visible="value => excerptVisible['在此'] = value"
+    >
+      <template #empty-message-addons>
+        <div
+          un-text="2xl neutral-800 dark:neutral-200"
+          un-my-10
+          un-flex="~ row"
+          un-justify-center
+        >
+          此处没有任何文章
+        </div>
+        <div
+          un-text="2xl neutral-700 dark:neutral-300"
+          un-my-10
+          un-flex="~ row"
+          un-justify-center
+        >
+          很可能是由于「{{ params?.tag }}」是一个泛类
+        </div>
+        <div
+          un-text="2xl neutral-600 dark:neutral-400"
+          un-my-10
+          un-flex="~ row"
+          un-justify-center
+        >
+          您需要看看
+          <LinkUnderline
+            un-align-base
+            href="#更深处"
+            text="更深处"
+            un-text="cyan-500"
+            un-before="bg-cyan-600 dark:bg-cyan-600/80"
+          />
+        </div>
+      </template>
+    </PostListSection>
+  </ClientOnly>
 
   <PostListSection
     category="更深处"
